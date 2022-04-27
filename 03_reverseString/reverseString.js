c8be6aeb7805d034c11d603d6378c95645609078
Adam Załęski <adamzaleski001@wp.pl>
2022-04-27 22:40:47 +0200

    Finished exe. number 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
+const reverseString = function(string) {
 
+    let revSrring = "";
+    string.split("").forEach(char => revSrring = char + revSrring);
+    return revSrring;
 };
 
 // Do not edit below this line
