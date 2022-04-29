15fefdc0f2ab869564eeac0aff60a8b20d9ffe25
Adam Załęski <adamzaleski001@wp.pl>
2022-04-29 11:25:41 +0200

    Finished tempConversion exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(temperature) {
+  const ans = (temperature - 32) * 5/9;  
+  return +(Math.round(ans + "e+1") + "e-1");
 };
 
-const ctof = function() {
-
+const ctof = function(temperature) {
+  const ans = (temperature * 9/5) + 32;
+  return +(Math.round(ans + "e+1") + "e-1");
 };
 
 // Do not edit below this line
