a2096e42643a8b704f6724add9025624633fd44d
Adam Załęski <adamzaleski001@wp.pl>
2022-04-29 11:06:42 +0200

    Finished  exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,15 @@
-const sumAll = function() {
+const sumAll = function(firstNum, secondNum) {
+    if (typeof firstNum !== 'number' || typeof secondNum !== 'number') {
+        return "ERROR";
+    }
+
+    const a_1 = Math.min(firstNum, secondNum);
+    const a_2 = Math.max(firstNum, secondNum);
+
+    if (a_2 <= 0 || a_1 <= 0){
+        return "ERROR";
+    }
+    return (a_1 + a_2)*(a_2 - a_1 + 1) / 2;
 
 };
 
