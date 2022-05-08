ad82433a70320740e76367df8eb6dcd6c2d16745
Adam Załęski <adamzaleski001@wp.pl>
2022-05-08 20:00:25 +0200

    Finished palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,14 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    let first = string
+            .toLowerCase()
+            .split('')
+            .filter((a) => a != '!' && a != ',' && a != '.')
+            .join('')
+            .replace(/ /g, "");
+    let second = first.split("").reverse().join("");
+    return first === second;
 };
+
 
 // Do not edit below this line
 module.exports = palindromes;
