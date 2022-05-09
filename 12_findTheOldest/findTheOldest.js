675de43c6a7d89790a307934c799cc297da83218
Adam Załęski <adamzaleski001@wp.pl>
2022-05-09 17:00:57 +0200

    Finished

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,25 @@
-const findTheOldest = function() {
+const findTheOldest = function(args) {    
 
+    const oldest = args.sort(function(a, b) {
+        let aEnd = 0;
+        let bEnd = 0;
+        if (!('yearOfDeath' in a)){
+            aEnd = new Date().getFullYear();
+        }
+        else{
+            aEnd = a.yearOfDeath;
+        }
+
+        if (!('yearOfDeath' in b)){
+            bEnd = new Date().getFullYear();
+        }
+        else{
+            bEnd = b.yearOfDeath;
+        }
+        return aEnd - a.yearOfBirth > bEnd - b.yearOfBirth ? -1: 1;
+        
+    });
+    return oldest[0];
 };
 
 // Do not edit below this line
