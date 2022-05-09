6b4327487941f20f89094733964eefc00e4e304a
Adam Załęski <adamzaleski001@wp.pl>
2022-05-09 16:03:39 +0200

    Finished fibonacci exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,16 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    n = parseInt(n);
+    if (n < 0) return 'OOPS';
+    const arr = new Array(n).fill(0);
+    arr[0] = 1;
+    for (let i = 0; i < n; i++) {
+        arr[i+1] += arr[i];        
+        arr[i+2] += arr[i];        
+    }
+    return arr[n - 1];
+
+
+
 
 };
 
